d12657c8ebab7269e69fc89d17944b2fad35930e
MatthieuOsten <91944066+MatthieuOsten@users.noreply.github.com>
2023-05-27 07:28:10 +0200

    new building secene version

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 
 public class LevelManager : MonoBehaviour
@@ -36,8 +37,16 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private List<int> buildingValues = new List<int>();
     [SerializeField] private Tilemap _groundTilemap;
     [SerializeField] private Tilemap _buildingTilemap;
+    [SerializeField] private Tilemap _previewTilemap;
     [SerializeField] private Camera _currentCamera;
     [SerializeField] private BuildingScriptable _allowedPlacement;
+    [SerializeField] private TileBase _actualTile;
+    [SerializeField] private int _prefabId;
+
+
+    [SerializeField] private Vector3Int currentGridPosition;
+    [SerializeField] private Vector3Int lastGridPosition;
+
     #endregion
 
     #region ACCESSEUR
@@ -49,12 +58,18 @@ public class LevelManager : MonoBehaviour
     #endregion
 
     #region FUNCTION UNITY
+
+    private void Start()
+    {
+        _actualTile = GetNewTile();
+
+        SceneManager.LoadScene("HUD",LoadSceneMode.Additive);
+    }
+
     private void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) && _actualTile != null)
         {
-            int rnd = UnityEngine.Random.Range(0, BuildingManager.Instance.buildingPrefabs.Count);
-            int rnd2 = UnityEngine.Random.Range(0, BuildingManager.Instance.buildingPrefabs[rnd].BuildingPrefabs.Count);
             Vector3 mousePos = _currentCamera.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int mousePosInt = _buildingTilemap.WorldToCell(mousePos);
             mousePosInt = new Vector3Int(
@@ -73,14 +88,48 @@ public class LevelManager : MonoBehaviour
             if (cnt == 0)
                 return;
             BuildingManager buildingManager = BuildingManager.Instance;
-            _buildingTilemap.SetTile(mousePosInt,
-                buildingManager.buildingPrefabs[rnd].BuildingPrefabs[rnd2]);
-            buildingManager.buildingPrefabs[rnd].OnPlace(_buildingTilemap, mousePosInt);
+            _buildingTilemap.SetTile(mousePosInt, _actualTile);
+            buildingManager.buildingPrefabs[_prefabId].OnPlace(_buildingTilemap, mousePosInt);
+
+            _actualTile = GetNewTile();
+        }
+        else if (_actualTile != null)
+        {
+            Vector3 _mousePos = _currentCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int _mousePosInt = _previewTilemap.WorldToCell(_mousePos);
+
+            _mousePosInt = new Vector3Int(
+                _mousePosInt.x,
+                _mousePosInt.y,
+                Mathf.RoundToInt(_previewTilemap.transform.position.z)
+                );
+
+            Vector3 pos = _currentCamera.ScreenToWorldPoint(_mousePos);
+            Vector3Int gridPos = _previewTilemap.WorldToCell(pos);
+
+            if (gridPos != currentGridPosition)
+            {
+                lastGridPosition = currentGridPosition;
+                currentGridPosition = gridPos;
+
+                UpdatePreview();
+
+            }
         }
     }
     #endregion
 
     #region FUNCTION
+
+    public TileBase GetNewTile()
+    {
+        int rnd = UnityEngine.Random.Range(0, BuildingManager.Instance.buildingPrefabs.Count);
+        int rnd2 = UnityEngine.Random.Range(0, BuildingManager.Instance.buildingPrefabs[rnd].BuildingPrefabs.Count);
+
+        _prefabId = rnd;
+
+        return BuildingManager.Instance.buildingPrefabs[rnd].BuildingPrefabs[rnd2];
+    }
 
     public int[] GetNeighboursTiles(Tilemap map, Vector3Int pos)
     {
@@ -104,6 +153,37 @@ public class LevelManager : MonoBehaviour
             return 2;
         return -1;
     }
+
+    public bool AllowedPlacement(Vector3Int mousePos)
+    {
+        int cnt = 0;
+        foreach (TileBase tile in _allowedPlacement.BuildingPrefabs)
+        {
+            if (_groundTilemap.GetTile(mousePos) == tile)
+                cnt++;
+        }
+        if (cnt == 0)
+            return false;
+
+        return true;
+    }
+
+    public bool ValitadeConstruct(Vector3Int mousePos)
+    {
+        if (!_groundTilemap.GetTile(mousePos) || _buildingTilemap.GetTile(mousePos))
+            return false;
+
+        return true;
+    }
+
+    private void UpdatePreview()
+    {
+        // Remove old tile if existing
+        _previewTilemap.SetTile(lastGridPosition, null);
+        // Set current tile to current mouse positions tile
+        _previewTilemap.SetTile(currentGridPosition, _actualTile);
+    }
+
     #endregion
 
 }
